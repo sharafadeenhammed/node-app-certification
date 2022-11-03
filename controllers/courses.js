72e5ab17995deb1c5cfa932953404151db8d64c1
Sharafadeen Hammed <allahsegun@gmail.com>
2022-11-03 15:18:14 +0100

    added update,delete,create functionalty to the course controller and router

--- controllers/courses.js
+++ controllers/courses.js
@@ -37,7 +37,6 @@ exports.getCourse = asyncHandeler(async function (req,res,next){
         select:"name description"
     }); 
     if(!course){
-        console.log("course error");
         return next(new errorResponse(`No courses with the id of ${req.params.id}`,404));
     }
     res.status(200).json({
@@ -56,16 +55,63 @@ exports.addCourse = asyncHandeler(async function (req,res,next){
     const bootcamp =  await Bootcamp.findById(req.params.bootcampId);
     if(!bootcamp){
         // return next(new errorResponse(`No Bootcamp with the id of ${req.params.bootcampId}`,404));
-        return next(new errorResponse("no matching bootcamp",404))
+        return next(new errorResponse(`No Bootcamp With The Id of ${req.params.bootcampId}`,404))
     }
     const course = await Course.create(req.body);
+    const courseCount =  await Course.count();
+    // console.log(courseCount);
     if(!course){
         return next(new errorResponse("cannot create course",500))
     }
-    res.status(200).json({
+    res.status(201).json({
         success:true,
         data:course
     })
     next();
-})
+});
+
+//@desc     Update a Course...
+//@route    UPDATE /api/v1/bootcamp/:bootcampId/courses
+//@access   Private.
+exports.updateCourse = asyncHandeler(async function (req,res,next){
+    let course = await Course.findById(req.params.id);
+    if(!course){
+        return next(new errorResponse(`No Course With The Id of ${req.params.id}`,404));
+    }
+     course = await Course.findByIdAndUpdate(req.params.id,req.body,{
+        runValidators:true,
+        new:true
+     })
+    if(!course){
+        return next(new errorResponse("Cannot Update course",500));
+    }
+    res.status(201).json({
+        success:true,
+        data:course
+    })
+    next();
+});
+
+//@desc     Delete a Course...
+//@route    DELETE /api/v1/bootcamp/:bootcampId/courses
+//@access   Private.
+exports.deleteCourse = asyncHandeler(async function (req,res,next){
+    const course = await Course.findById(req.params.id);
+    if(!course){
+        return next(new errorResponse(`No Course With The Id of ${req.params.id}`,404));
+    }
+    course.remove();
+    res.status(201).json({
+        success:true,
+        data:course
+    })
+    next();
+});
+
+
+    /*
+    dad ridwan account detiails...
+    "0119368764"
+    gtbank
+    */
 
--- routes/courses.js
+++ routes/courses.js
@@ -4,10 +4,12 @@ const {
     getCourses,
     getCourse,
     addCourse,
+    updateCourse,
+    deleteCourse,
 } = require("../controllers/courses");
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 
 module.exports = router;
